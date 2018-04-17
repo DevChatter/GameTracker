181e51583d61d32ec2898a47c3fd62f9928ce355
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-17 15:20:26 -0400

    Adding viewmodel creation page for gamerecords

---
+++ src/DevChatter.GameTracker/Pages/GameRecords/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using DevChatter.GameTracker.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevChatter.GameTracker.Pages.GameRecords
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public GameRecordViewModel GameRecord { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
