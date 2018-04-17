6ebe2c0921ed5f0a815e31d6cde0416349840d9d
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-17 14:39:08 -0400

    Adding Migration for GameRecords and PlayRecords

--- src/DevChatter.GameTracker.Data.Ef/Migrations/20180417183737_CreateGameRecordModels.Designer.cs
+++ src/DevChatter.GameTracker.Data.Ef/Migrations/20180417183737_CreateGameRecordModels.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DevChatter.GameTracker.Data.Ef.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180417183737_CreateGameRecordModels")]
+    partial class CreateGameRecordModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
