49266b569fa6c99f89c963d97a91fcde15fdf4b0
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-17 13:13:31 -0400

    Adding Repository and Specification interfaces

---
+++ src/DevChatter.GameTracker.Core/Data/Specifications/BaseEntityPolicy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using DevChatter.GameTracker.Core.Model;
+
+namespace DevChatter.GameTracker.Core.Data.Specifications
+{
+    public class BaseEntityPolicy<T> : ISpecification<T> where T : BaseEntity
+    {
+        public BaseEntityPolicy(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public static BaseEntityPolicy<T> All()
+        {
+            return new BaseEntityPolicy<T>(x => true);
+        }
+
+        public static BaseEntityPolicy<T> ById(Guid id)
+        {
+            return new BaseEntityPolicy<T>(x => x.Id == id);
+        }
+
+        public Expression<Func<T, bool>> Criteria { get; }
+    }
+}
