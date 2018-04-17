2269e54bc12b59e1db6747b154a191380412c4f6
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-17 13:16:33 -0400

    Adding EfCore project

---
+++ src/DevChatter.GameTracker.Data.Ef/EfCoreRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using DevChatter.GameTracker.Core.Data;
+using DevChatter.GameTracker.Core.Data.Specifications;
+using DevChatter.GameTracker.Core.Model;
+
+namespace DevChatter.GameTracker.Data.Ef
+{
+    public class EfCoreRepository : IRepository
+    {
+
+        public T Single<T>(ISpecification<T> spec) where T : BaseEntity
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<T> List<T>(ISpecification<T> spec = null) where T : BaseEntity
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Create<T>(T dataItem) where T : BaseEntity
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Update<T>(T dataItem) where T : BaseEntity
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove<T>(T dataItem) where T : BaseEntity
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
