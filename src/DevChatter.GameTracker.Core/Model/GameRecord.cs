23757e28b8900c79af7794f31c9afe5601cef0f9
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-17 14:34:16 -0400

    modeling the play records

---
+++ src/DevChatter.GameTracker.Core/Model/GameRecord.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DevChatter.GameTracker.Core.Model
+{
+    public class GameRecord : BaseEntity
+    {
+        public Game Game { get; set; }
+        public List<PlayRecord> PlayRecords { get; set; }
+    }
+}
