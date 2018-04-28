71bb3a02b3072821ef9f9cdf0c0f9e909c57bdc3
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-28 15:28:10 -0400

    Remove the exact check

--- src/DevChatter.GameTracker.Infra.Bgg/GameDataService.cs
+++ src/DevChatter.GameTracker.Infra.Bgg/GameDataService.cs
@@ -22,7 +22,7 @@ namespace DevChatter.GameTracker.Infra.Bgg
                 client.BaseAddress = new Uri(bggBaseUrl);
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
 
-                string apiSearchQuery = string.Format("search?query={0}&exact=1", gameTitle);
+                string apiSearchQuery = string.Format("search?query={0}", gameTitle);
 
                 var result = client.GetAsync(apiSearchQuery).Result;
 
