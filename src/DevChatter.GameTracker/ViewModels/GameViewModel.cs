f5c8766c716436fe04d630b0aef626b138d2c8fc
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-28 13:36:08 -0400

    Code Clean Up

---
+++ src/DevChatter.GameTracker/ViewModels/GameViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DevChatter.GameTracker.ViewModels
+{
+    public class GameViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string BoardGameGeekLink { get; set; }
+    }
+}
