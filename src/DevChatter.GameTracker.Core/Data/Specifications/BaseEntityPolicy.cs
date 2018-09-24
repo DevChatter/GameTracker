9ede260d500a99b5dcdedbd2a09ad2e7921e6556
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-09-24 15:25:14 -0400

    Move PlayerPolicy to own file

--- src/DevChatter.GameTracker.Core/Data/Specifications/BaseEntityPolicy.cs
+++ src/DevChatter.GameTracker.Core/Data/Specifications/BaseEntityPolicy.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Linq.Expressions;
 using DevChatter.GameTracker.Core.Model;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -27,26 +26,5 @@ namespace DevChatter.GameTracker.Core.Data.Specifications
         }
 
         public Expression<Func<T, bool>> Criteria { get; }
-    }
-
-    public class PlayerPolicy : BaseEntityPolicy<Player>
-    {
-        protected PlayerPolicy(Expression<Func<Player, bool>> criteria) 
-            : base(criteria)
-        {
-        }
-
-        /// <summary>
-        /// Gets names similar to the specified criteria.
-        /// Ex: "Brend" would find ["Brendan", "Brendon", "Brenden"]
-        /// </summary>
-        public static PlayerPolicy ByNameLike(string filter)
-        {
-            string[] searchTerms = filter.ToLowerInvariant().Split(" ",
-                StringSplitOptions.RemoveEmptyEntries);
-            return new PlayerPolicy(p =>
-                searchTerms.All(term => p.FirstName.ToLowerInvariant().StartsWith(term)
-                                     || p.LastName.ToLowerInvariant().StartsWith(term)));
-        }
     }
 }
