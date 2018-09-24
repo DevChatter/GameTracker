1dca29e054f1d5f4e2a2201d8760a07541af4b22
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-09-24 15:07:46 -0400

    Creating a Player API Controller

--- src/DevChatter.GameTracker.Core/Data/Specifications/BaseEntityPolicy.cs
+++ src/DevChatter.GameTracker.Core/Data/Specifications/BaseEntityPolicy.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using DevChatter.GameTracker.Core.Model;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace DevChatter.GameTracker.Core.Data.Specifications
 {
@@ -25,5 +27,26 @@ namespace DevChatter.GameTracker.Core.Data.Specifications
         }
 
         public Expression<Func<T, bool>> Criteria { get; }
+    }
+
+    public class PlayerPolicy : BaseEntityPolicy<Player>
+    {
+        protected PlayerPolicy(Expression<Func<Player, bool>> criteria) 
+            : base(criteria)
+        {
+        }
+
+        /// <summary>
+        /// Gets names similar to the specified criteria.
+        /// Ex: "Brend" would find ["Brendan", "Brendon", "Brenden"]
+        /// </summary>
+        public static PlayerPolicy ByNameLike(string filter)
+        {
+            string[] searchTerms = filter.ToLowerInvariant().Split(" ",
+                StringSplitOptions.RemoveEmptyEntries);
+            return new PlayerPolicy(p =>
+                searchTerms.All(term => p.FirstName.ToLowerInvariant().StartsWith(term)
+                                     || p.LastName.ToLowerInvariant().StartsWith(term)));
+        }
     }
 }
---
+++ src/DevChatter.GameTracker/Controllers/PlayersController.cs
@@ -0,0 +1,61 @@
+﻿using DevChatter.GameTracker.Core.Data;
+using DevChatter.GameTracker.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevChatter.GameTracker.Core.Data.Specifications;
+
+namespace DevChatter.GameTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private readonly IRepository _repository;
+
+        public PlayersController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/Players
+        [HttpGet]
+        public IEnumerable<Player> GetPlayers()
+        {
+            return _repository.List<Player>();
+        }
+
+        // GET: api/Players/Brend
+        [HttpGet("{filter}")]
+        public IActionResult SearchPlayers([FromRoute] string filter)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var player = _repository.List(PlayerPolicy.ByNameLike(filter));
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(player);
+        }
+
+        // POST: api/Players
+        [HttpPost]
+        public async Task<IActionResult> PostPlayer([FromBody] Player player)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            player = _repository.Create(player);
+
+            return CreatedAtAction("GetPlayer", new { id = player.Id }, player);
+        }
+    }
+}
