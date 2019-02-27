3af6ee9f342db6b27f206dcb332e2e0062dfb998
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-02-27 17:38:45 -0500

    Removing unused file

--- src/DevChatter.GameTracker/ViewModels/CreatePlayerViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevChatter.GameTracker.ViewModels
-{
-    public class CreatePlayerViewModel
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
